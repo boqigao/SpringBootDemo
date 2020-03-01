a6ac0634c8b4ef2b0156295f4640d954424d193e
gaoboqi <gaoboqi@gmail.com>
2020-03-01 16:33:41 +0900

    2020/3/1

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//ResponseBody， 这个类的所有方法返回的数据直接写给浏览器，（如果是对象还能转为json对象）
+
+@RestController
 public class HelloController {
 
-    @ResponseBody
+
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * SpringBoot的单元测试
+ *
+ * 可以再测试期间很方便的蕾丝边马一样进行自动注入的容器的功能
+ */
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	Person person;
+
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
